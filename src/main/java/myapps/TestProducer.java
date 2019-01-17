9401913fd6a762eae04f47602b8e9bf07bf0dbdb
Pieter Polmans <trianoware@gmail.com>
2019-01-17 17:34:48 +0100

    added a test producer, may god watch over us

---
+++ src/main/java/myapps/TestProducer.java
@@ -0,0 +1,33 @@
+package myapps;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
+public class TestProducer {
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        for(int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("streams-plaintext-input",
+                    Integer.toString(i), Integer.toString(i)));
+
+        }
+
+        producer.close();
+    }
+}
