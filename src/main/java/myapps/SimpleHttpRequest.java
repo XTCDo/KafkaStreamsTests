c0700e5d1b424a344f562d8b4c208301da325102
Pieter Polmans <trianoware@gmail.com>
2019-01-21 17:20:10 +0100

    fixed typo

--- src/main/java/myapps/SimpleHttpRequest.java
+++ src/main/java/myapps/SimpleHttpRequest.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class SimpleHtttpRequest {
+public class SimpleHttpRequest {
     public static void main(String[] args){
 
         try {
