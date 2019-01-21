25f8d7c0531fd5e0cd1ce573a8acf10af4ae75c2
Pieter Polmans <trianoware@gmail.com>
2019-01-21 16:08:16 +0100

    changed deserializer to something that should work

--- src/main/java/myapps/BasicConsumer.java
+++ src/main/java/myapps/BasicConsumer.java
@@ -2,6 +2,7 @@ package myapps;
 
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -10,8 +11,8 @@ public class BasicConsumer {
     public static void main(String[] args){
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
         final Consumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 
