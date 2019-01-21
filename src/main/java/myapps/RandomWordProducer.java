c78d96dbe8565cb0f9cdbe7d11413bb71597f05b
Pieter Polmans <trianoware@gmail.com>
2019-01-21 14:21:55 +0100

    Fixed generated word not being reset

--- src/main/java/myapps/RandomWordProducer.java
+++ src/main/java/myapps/RandomWordProducer.java
@@ -33,8 +33,8 @@ public class RandomWordProducer {
                     final int MIN_WORDS = 1;
                     final int MAX_WORDS = WORDS.length;
 
-                    String generatedWord = "";
                     while(true){
+                        String generatedWord = "";
                         for(int i = MIN_WORDS; i < MAX_WORDS - 1; i++){
                             generatedWord += " ";
                             generatedWord += WORDS[randomInteger(0, WORDS.length - 1)];
