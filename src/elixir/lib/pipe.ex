46fdedf9e7b492da961729b80e3b3ca6bb03d3c1
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-21 15:11:33 +0100

    added beasic elixr code for testing

---
+++ src/elixir/lib/pipe.ex
@@ -0,0 +1,15 @@
+defmodule Pipe do
+  @moduledoc false
+  use KafkaEx.GenConsumer
+  alias kafkaEx.Protocol.Fetch.Message
+
+  require Logger
+
+  #note: messages are delivered in batches -> check out OTP + KafkaEx or ask Chiel/Argu
+  def handle_message_set(message_set, state) do
+    for %Message{value: message} <- message_set do
+      Logger.debug(fn-> "message: " <> inspect(message) end )
+      end
+    {:async_commit, state}
+  end
+end
