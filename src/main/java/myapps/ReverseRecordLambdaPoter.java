521be896653e7089bf54c87e15a7efb68f0e705b
Pieter Polmans <trianoware@gmail.com>
2019-01-21 11:51:33 +0100

    fixed

--- src/main/java/myapps/ReverseRecordLambdaPoter.java
+++ src/main/java/myapps/ReverseRecordLambdaPoter.java
@@ -36,7 +36,7 @@ public class ReverseRecordLambdaPoter {
 
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-reverserecord");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-reverserecordpoter");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -53,7 +53,9 @@ public class ReverseRecordLambdaPoter {
                 outputAsCharArray[inputAsCharArray.length - 1 - i] = inputAsCharArray[i];
             }
             return new String(outputAsCharArray);
-        });
+        }).to("streams-reverserecordpoter-output");
+
+
 
         final Topology topology = builder.build();
 		System.out.println(topology.describe());
