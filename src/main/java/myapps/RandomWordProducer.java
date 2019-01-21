a4042d3edac676ee7a461345add31a4b8f207dfc
Pieter Polmans <trianoware@gmail.com>
2019-01-21 14:12:47 +0100

    Added random word producer

--- src/main/java/myapps/RandomWordProducer.java
+++ src/main/java/myapps/RandomWordProducer.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 public class RandomWordProducer {
     public static void main(String[] args) throws Exception {
@@ -22,14 +23,50 @@ public class RandomWordProducer {
         props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
 
         Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        for(int i = 0; i < 100; i++) {
-			int randomInteger = randomInteger(0, 100);
-            producer.send(new ProducerRecord<String, String>("streams-plaintext-input",
-                    Integer.toString(randomInteger), Integer.toString(randomInteger)));
 
+        Thread producerThread = new Thread(){
+            public void run(){
+                try {
+                    final String[] WORDS = {
+                        "test", "random", "word", "java", "kafka", "apache"
+                    };
+                    final int MIN_WORDS = 1;
+                    final int MAX_WORDS = WORDS.length;
+
+                    String generatedWord = "";
+                    while(true){
+                        for(int i = MIN_WORDS; i < MAX_WORDS - 1; i++){
+                            generatedWord += " ";
+                            generatedWord += WORDS[randomInteger(0, WORDS.length - 1)];
+                        }
+                        producer.send(new ProducerRecord<String, String>("streams-plaintext-input",
+                                generatedWord, generatedWord));
+                        Thread.sleep(1000);
+                    }
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook"){
+            @Override
+            public void run(){
+                producer.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            producerThread.start();
+            latch.await();
+        } catch (Throwable e){
+            System.exit(1);
         }
 
-        producer.close();
+        System.exit(0);
     }
 
 	private static int randomInteger(int min, int max) {
