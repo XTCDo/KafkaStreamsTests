84065e7987390dcb5836690e2fe81af4b4dafbd1
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-22 11:46:25 +0100

    infuxWrapper now has some more setup and a ParamStringBuilder was added

--- src/main/java/myapps/InfluxWrapper.java
+++ src/main/java/myapps/InfluxWrapper.java
@@ -1,18 +1,46 @@
 package myapps;
 
 
+import java.io.DataOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InfluxWrapper {
     // http naar localhost:8086
     // db = kafka test
     // get request
 
-    // d--data-urlencode 'q=SELECT * FROM weather'
+    // --data-urlencode 'q=SELECT * FROM weather'
+
     public static void main(String[] args){
-        URL url = new URL("http://[::1]:8086");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
+        try {
+            // set up connection to localhost URL
+            URL url = new URL("http://localhost:8086/query?");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+
+            // request params
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("db","kafka_test");
+            parameters.put("q", "SELECT * FROM weather");
+
+            // request is url-encoded
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            con.setDoOutput(true);
+            DataOutputStream out = new DataOutputStream(con.getOutputStream());
+            out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
+            out.flush();
+            out.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
+
