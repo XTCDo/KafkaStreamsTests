78e632e66ea01244bf9976a301fdec00430eb60e
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-22 11:51:02 +0100

    added response handling, hopefully

--- src/main/java/myapps/InfluxWrapper.java
+++ src/main/java/myapps/InfluxWrapper.java
@@ -1,7 +1,9 @@
 package myapps;
 
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -11,12 +13,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class InfluxWrapper {
-    // http naar localhost:8086
-    // db = kafka test
-    // get request
-
-    // --data-urlencode 'q=SELECT * FROM weather'
-
     public static void main(String[] args){
         try {
             // set up connection to localhost URL
@@ -32,11 +28,30 @@ public class InfluxWrapper {
             // request is url-encoded
             con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
+            // do the request, specified by out
             con.setDoOutput(true);
             DataOutputStream out = new DataOutputStream(con.getOutputStream());
             out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
             out.flush();
             out.close();
+
+            // do something with the response specified by in
+            int status = con.getResponseCode();
+            switch (status) {
+                case 200:
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer content = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
+                    }
+                    in.close();
+                    System.out.println(content);
+                default:
+                    System.out.println(status);
+            }
+
 
         } catch (Exception e){
             e.printStackTrace();
