54f51a885e75f906ab3ae7898b240be72dfb2d6c
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-22 12:05:04 +0100

    lunch breakd

--- src/main/java/myapps/InfluxWrapper.java
+++ src/main/java/myapps/InfluxWrapper.java
@@ -4,58 +4,51 @@ package myapps;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
 public class InfluxWrapper {
-    public static void main(String[] args){
-        try {
-            // set up connection to localhost URL
-            URL url = new URL("http://localhost:8086/query?");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("POST");
+    public static void query(String baseUrl, String database, String query)
+    throws Exception {
+        // set up connection to localhost URL
+        HttpURLConnection con = (HttpURLConnection) new URL(baseUrl + "/query?").openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-            // request params
-            Map<String, String> parameters = new HashMap<>();
-            parameters.put("db","kafka_test");
-            parameters.put("q", "SELECT * FROM weather");
+        // set up request params
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("db",database);
+        parameters.put("q", query);
 
-            // request is url-encoded
-            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        // perform HTTP request
+        con.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
+        out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
+        out.flush();
+        out.close();
 
-            // do the request, specified by out
-            con.setDoOutput(true);
-            DataOutputStream out = new DataOutputStream(con.getOutputStream());
-            out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
-            out.flush();
-            out.close();
+    }
 
-            // do something with the response specified by in
-            int status = con.getResponseCode();
-                if (status<300){ // 2XX is okay range for HTTP
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(con.getInputStream()));
-                    String inputLine;
-                    StringBuffer content = new StringBuffer();
-                    while ((inputLine = in.readLine()) != null) {
-                        content.append(inputLine);
-                    }
-                    in.close();
-                    System.out.println(content);
 
-            }else{
-                System.out.println(status);
+    public void processResponse(HttpURLConnection con) throws Exception{
+        int status = con.getResponseCode();
+        if (status<300){ // 2XX is okay range for HTTP
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
             }
+            in.close();
+            System.out.println(content);
 
-            con.disconnect();
-        } catch (Exception e){
-            e.printStackTrace();
+        }else{
+            System.out.println(status);
         }
+
+        con.disconnect();
     }
 }
-
