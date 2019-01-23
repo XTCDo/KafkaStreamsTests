8b2f1a26178b95bc2907b304f96742b2408e85f5
Pieter Polmans <trianoware@gmail.com>
2019-01-23 10:16:46 +0100

    Changed some of the query records to see if that fixes our problem

--- src/main/java/myapps/InfluxDAOTest.java
+++ src/main/java/myapps/InfluxDAOTest.java
@@ -7,8 +7,8 @@ public class InfluxDAOTest {
     public static void main(String[] args){
         InfluxDAO dao = new InfluxDAO("http://localhost:8086");
         Map<String, String> data = new HashMap<String, String>();
+        data.put("temperature", "0");
         data.put("location", "russia");
-        data.put("temperature", "-500");
         dao.insertRecord("kafka_test", "weather", data);
     }
 }
