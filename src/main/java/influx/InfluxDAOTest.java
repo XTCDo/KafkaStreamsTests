406a4bdc1e255447a87cba04e8719bd09917f1db
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-23 14:25:21 +0100

    typo fix

--- src/main/java/influx/InfluxDAOTest.java
+++ src/main/java/influx/InfluxDAOTest.java
@@ -13,7 +13,7 @@ public class InfluxDAOTest {
 
         // === testing basic query ===
         dao.query("kafka_test", "SELECT * FROM weather");
-        dao.oldquery("kafka_test","SELECT * FROM planets");
+        dao.query("kafka_test","SELECT * FROM planets");
 
         // === testing insert fun===
         // Make some tags
