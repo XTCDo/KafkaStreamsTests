9a0bccea91059bd868b4e2acd489933774119df2
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-23 13:27:51 +0100

    testing simple insertion

--- src/main/java/influx/InfluxDAOTest.java
+++ src/main/java/influx/InfluxDAOTest.java
@@ -20,5 +20,34 @@ public class InfluxDAOTest {
         System.out.println("Inserting");
         dao.insertRecord("kafka_test", "weather", tags, fields);
         System.out.println("Done.");
+
+        // testing simple insert and Record together
+        Record earth= new Record();
+        earth.addTag("location","Terra");
+        earth.addTag("color","light blue");
+
+        earth.addField("dist_to_sun",1); // in AU
+        earth.addField("gravity", 9.81);
+        earth.addField("temperature",20);
+
+        Record mars= new Record();
+        mars.addTag("location","Mars");
+        mars.addTag("color","red brown");
+
+        mars.addField("gravity", 9.81/3);
+        mars.addField("temperature",-150);
+        mars.addField("dist_to_sun",1.5);
+
+        Record venus= new Record();
+        venus.addTag("location","Venus");
+        venus.addTag("color", "white");
+
+        venus.addField("gravity", 8.87);
+        venus.addField("temperature", 400);
+        venus.addField("dist_to_sun",0.723);
+
+        dao.simpleInsertRecord("kafka_test","planets",earth);
+        dao.simpleInsertRecord("kafka_test","planets",mars);
+        dao.simpleInsertRecord("kafka_test","planets",venus);
     }
 }
