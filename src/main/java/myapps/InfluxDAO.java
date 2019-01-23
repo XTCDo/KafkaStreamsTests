ef84f2b9818203558cfc1bfeb5ff5a23957139d9
Pieter Polmans <trianoware@gmail.com>
2019-01-23 11:17:09 +0100

    fixed something i forgot

--- src/main/java/myapps/InfluxDAO.java
+++ src/main/java/myapps/InfluxDAO.java
@@ -60,8 +60,7 @@ public class InfluxDAO {
 
         return responseStringBuilder.toString();
     }
-
-<<<<<<< HEAD
+    
     public boolean insertRecord(String database, String table, Map<String, String> tags, Map<String, String> fields){
         //"INSERT weather,location=us-midwest temperature=8"
         try {
