1eb9348abc4b356d717a8f3d3f2d086313030e97
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-24 14:01:38 +0100

    added basic logging functions

--- src/main/java/util/Logging.java
+++ src/main/java/util/Logging.java
@@ -1,28 +1,59 @@
 package util;
 
+import org.apache.kafka.common.protocol.types.Field;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 
 public class Logging {
+    private static final Logger logger = Logger.getLogger(Logging.class.getName());
+    private static final String TAG = "logging";
 
-    // === public functs ===
+    // public functions
 
     /**
-     *
+     * print a formatted string directly to System.out instead of a file
      * @param tag
      * @param message
      */
     public static void logprint(String tag, String message){
-        System.out.println(fullmessage(tag,message));
+        System.out.println(fullMessage(tag,message));
     }
 
-    public static void log(){
+    public static void info(String tag, String message){info(fullMessage(tag,message));}
+    public static void warn(String tag, String message){warn(fullMessage(tag,message));}
+    public static void error(String tag, String message){error(fullMessage(tag,message));}
+    public static void debug(String tag, String message){debug(fullMessage(tag,message));}
 
+    public static void info(String fullmessage){logger.info(fullmessage);}
+    public static void warn(String fullmessage){logger.warning(fullmessage);}
+    public static void error(String fullmessage){logger.severe(fullmessage);}
+    public static void debug(String fullmessage){logger.fine(fullmessage);}
+
+
+    /**
+     *  general log call for personal preference or other levels
+     * @param level
+     * @param tag
+     * @param message
+     */
+    public static void log(Level level, String tag, String message){
+        String fullMessage = fullMessage(tag,message);
+        try {
+            logger.log(level, fullMessage);
+        } catch (Exception e){
+            error(fullMessage(TAG, "exception while trying to log, defaulting to info"));
+            info(fullMessage);
+        }
     }
 
-    // === private functions ===
+
+    // private functions
 
     /**
      * converts circumstantial parameters to a timestamed, printable message
@@ -30,7 +61,7 @@ public class Logging {
      * @param message
      * @return
      */
-    private static String fullmessage(String tag, String message){
+    private static String fullMessage(String tag, String message){
         return new StringBuilder()
                 .append("<").append(now()).append("> ")
                 .append("[").append(tag).append("]: ")
@@ -42,33 +73,3 @@ public class Logging {
         return new SimpleDateFormat("HH:mm:ss").format(new Date());
     }
 }
-
-import logging
-        from time import strftime, localtime
-
-
-        # logging function that provides timestamps and tags for debugging
-        def log(level, tag, message):
-        try:
-        tag = str(tag)
-        message = str(message)
-        time_stamp = strftime("%H:%M:%S", localtime())
-        # levels are defined in library API
-        level = str(level).lower()
-        if level in ["c", "crit", "critical"]:
-        level = 50
-        elif level in ["e", "err", "error"]:
-        level = 40
-        elif level in ["w", "warn", "warning"]:
-        level = 30
-        elif level in ["i", "info"]:
-        level = 20
-        elif level in ["d", "debug"]:
-        level = 10
-        else:
-        level = 0
-        logging.log(level, "<%s> [%s]: %s" % (time_stamp, tag, message))
-        # when exceptions happen, don't hold on to them but throw them in the caller's face
-        # so they can properly handle them
-        except Exception as e:
-        raise Exception(e)
