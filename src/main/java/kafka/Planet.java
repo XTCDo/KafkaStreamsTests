037872e5234d200de66b8510d78378eda8ba502d
Pieter Polmans <trianoware@gmail.com>
2019-01-24 10:46:17 +0100

    Added Planets and PlanetProducer

---
+++ src/main/java/kafka/Planet.java
@@ -0,0 +1,68 @@
+package kafka;
+
+public class Planet {
+    private String name;
+    private String capitol;
+    private String color;
+    private float distanceToSun;
+    private float gravity;
+    private float temperature;
+
+    public Planet(String name, String capitol, String color, float distanceToSun, float gravity, float temperature) {
+        this.setName(name);
+        this.setCapitol(capitol);
+        this.setColor(color);
+        this.setDistanceToSun(distanceToSun);
+        this.setGravity(gravity);
+        this.setTemperature(temperature);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapitol() {
+        return capitol;
+    }
+
+    public void setCapitol(String capitol) {
+        this.capitol = capitol;
+    }
+
+    public float getDistanceToSun() {
+        return distanceToSun;
+    }
+
+    public void setDistanceToSun(float distanceToSun) {
+        this.distanceToSun = distanceToSun;
+    }
+
+    public float getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(float gravity) {
+        this.gravity = gravity;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/main/java/kafka/PlanetProducer.java
@@ -0,0 +1,133 @@
+package kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+
+public class PlanetProducer {
+    public static void main(String[] args){
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+
+        Producer<String, Planet> producer = new KafkaProducer<String, Planet>(props);
+
+        Thread producerThread = new Thread(() -> {
+            try {
+                List<Planet> planets = new ArrayList<>();
+                planets.add(new Planet(
+                        "Mercury",
+                        "Sydney",
+                        "brown",
+                        0.39f,
+                        3.7f,
+                        452
+                        ));
+                planets.add(new Planet(
+                        "Venus",
+                        "",
+                        "light_blue",
+                        0.723f,
+                        8.87f,
+                        726
+                ));
+                planets.add(new Planet(
+                        "Terra",
+                        "Diepenbeek",
+                        "blue",
+                        1f,
+                        9.81f,
+                        310
+                ));
+                planets.add(new Planet(
+                        "Mars",
+                        "Valis Marineris",
+                        "red/brown",
+                        1.524f,
+                        3.711f,
+                        150
+                ));
+                planets.add(new Planet(
+                        "Jupiter",
+                        "",
+                        "brown",
+                        5.203f,
+                        24.79f,
+                        120
+                ));
+                planets.add(new Planet(
+                        "Saturn",
+                        "",
+                        "light_brown",
+                        9.539f,
+                        10.44f,
+                        88
+                ));
+                planets.add(new Planet(
+                        "Uranus",
+                        "",
+                        "white_blue",
+                        19.18f,
+                        8.87f,
+                        59
+                ));
+                planets.add(new Planet(
+                        "Neptune",
+                        "",
+                        "dark_blue",
+                        30.06f,
+                        11.15f,
+                        48
+                ));
+                planets.add(new Planet(
+                        "Pluto",
+                        "",
+                        "grey/brown",
+                        39.53f,
+                        0.62f,
+                        37
+                ));
+
+                while(true) {
+                    for(Planet p : planets){
+                        producer.send(new ProducerRecord<String, Planet>("streams-planets-input",
+                                p.getName(), p));
+                    }
+                    Thread.sleep(1000);
+                }
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook"){
+            @Override
+            public void run(){
+                producer.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            producerThread.start();
+            latch.await();
+        } catch(Throwable e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+}
