97704e912e29199e9b25a20c1e735a8154cc7f85
Pieter Polmans <trianoware@gmail.com>
2019-01-24 11:37:47 +0100

    prettier print

--- src/main/java/kafka/PlanetConsumer.java
+++ src/main/java/kafka/PlanetConsumer.java
@@ -30,10 +30,10 @@ public class PlanetConsumer {
                 String[] data = value.split(":");
                 System.out.printf("Planet: %s\n", data[0]);
                 System.out.printf("\tCapitol:\t%s\n", data[1]);
-                System.out.printf("\tColor:\t%s\n", data[2]);
+                System.out.printf("\tColor:\t\t\t%s\n", data[2]);
                 System.out.printf("\tDistance to Sun:\t%s AU\n", data[3]);
-                System.out.printf("\tGravity:\t%s m/s^2\n", data[4]);
-                System.out.printf("\tTemperature:\t%s K\n", data[5]);
+                System.out.printf("\tGravity:\t\t%s m/s^2\n", data[4]);
+                System.out.printf("\tTemperature:\t\t%s K\n", data[5]);
             }
         }
     }
