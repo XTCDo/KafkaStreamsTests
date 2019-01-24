7d26fff1d72adb4e441179519af0805f10f7631c
Pieter Polmans <trianoware@gmail.com>
2019-01-24 13:36:34 +0100

    added some comments and docs

--- src/main/java/kafka/PlanetConsumer.java
+++ src/main/java/kafka/PlanetConsumer.java
@@ -5,18 +5,15 @@ import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.influxdb.dto.Point;
 
-import javax.print.DocFlavor;
-import java.lang.reflect.Array;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class PlanetConsumer {
     public static void main(String[] args){
         Properties props = new Properties();
+        // Set properties
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "PlanetConsumer");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
@@ -24,12 +21,22 @@ public class PlanetConsumer {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
+        // Create consumer
         final Consumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 
+        // Subscribe to planets input topic
         consumer.subscribe(Arrays.asList("streams-planets-input"));
+
+        // Get dao object
         InfluxDAO dao = new InfluxDAO("http://localhost:8086");
+
+        // Keep consuming records for all eternity
         while(true){
+            // Poll for records
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10));
+
+            // Turn every record into a Planet, describe it and
+            // write it to Influx
             for(ConsumerRecord<String, String> record : records){
                 Planet planet = new Planet(record.value());
                 planet.describe();
@@ -39,6 +46,11 @@ public class PlanetConsumer {
         }
     }
 
+    /**
+     * Turns planets into Points
+     * @param planet The Planet to turn into a Point
+     * @return The Planet turned into a Point
+     */
     private static Point planetToPoint(Planet planet){
         Point point = Point.measurement("planets")
                 .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
