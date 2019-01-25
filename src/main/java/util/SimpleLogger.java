ef6f39b8bebd42136f453ad9db1dae9eeaa0f24a
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-25 16:38:12 +0100

    formatting

--- src/main/java/util/SimpleLogger.java
+++ src/main/java/util/SimpleLogger.java
@@ -9,7 +9,7 @@ public class SimpleLogger  {
     private static Logger logger = LoggerFactory.getLogger("logger");
 
     // calls to logging
-    public static void info(String message) {logger.info("{1}",message);}
+    public static void info(String message) {logger.info("{}",message);}
     public static void debug(String message) {logger.debug(message);}
     public static void warn(String message) { logger.warn(message);}
     public static void error(String message) {logger.error(message);}
