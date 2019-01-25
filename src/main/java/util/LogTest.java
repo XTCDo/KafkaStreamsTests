02ad5a1c76d5fc4775408af193eeda95ffe24adc
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-25 17:08:13 +0100

    oops

--- src/main/java/util/LogTest.java
+++ src/main/java/util/LogTest.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 public class LogTest {
     private static final Logger logger = Logger.getLogger("logger");
 
-    private static void main(String[] args ){
+    public static void main(String[] args ){
         for (int i=0; i< 10; i++){
             logger.info("test");
         }
