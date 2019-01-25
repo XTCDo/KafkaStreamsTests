99082e5d72f2185215081460a09141b68ca4fbea
Chiel Dommange <chiel.dommange@gmail.com>
2019-01-25 14:16:37 +0100

    stress testing logging

--- src/main/java/util/Logtest.java
+++ src/main/java/util/Logtest.java
@@ -1,21 +1,36 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 
 public class Logtest {
     private static final String TAG = "logtest";
 
     public static void main(String[] args){
-        Logging log = new Logging();
-        Logging log2 = new Logging();
-        Logging log3 = new Logging();
 
-        log.info(TAG,"starting up logtest");
-        log.warn(TAG, "oh no, something's wrong!");
-        log.log(Level.SEVERE,TAG,"Something has gone off the rails");
 
-        log.log(Level.INFO, "testing tagless logging");
+        // depth test
+        Level[] levelArray= new Level[]{Level.INFO, Level.WARNING, Level.SEVERE, Level.FINE, Level.FINER, Level.FINEST};
+
+
+
+        List<Logging> logList = new ArrayList<>();
+        logList.add(new Logging());
+        logList.add(new Logging(true, false));
+        logList.add(new Logging(false, true));
+
+        logList.forEach(logging -> {
+            Arrays.stream(levelArray).forEach(level -> logging.log(level, TAG, "testing "+level.getName()));
+        });
+
+        // stress test
+        for (int i=0; i<100000; i++){
+            logList.get(0).debug(TAG,"stress test iteration"+ i);
+        }
 
         Logging.logprint("console","just printing to console");
+
     }
 }
