1d27b95da3a06a025bf243147c3fe4092d47d768
Pieter Polmans <trianoware@gmail.com>
2019-01-25 13:16:58 +0100

    removed abstract classes and any related classes so I can start again

--- src/main/java/kafka/PlanetProducer.java
+++ src/main/java/kafka/PlanetProducer.java
@@ -27,7 +27,7 @@ public class PlanetProducer {
 
         Thread producerThread = new Thread(() -> {
             try {
-                List<Planet> planets = getPlanets();
+                List<Planet> planets = PlanetProvider.getPlanets();
 
                 while(true) {
                     for(Planet p : planets){
@@ -62,80 +62,4 @@ public class PlanetProducer {
         System.exit(0);
     }
 
-    private static List<Planet> getPlanets(){
-        List<Planet> planets = new ArrayList<>();
-        planets.add(new Planet(
-                "Mercury",
-                "Sydney",
-                "brown",
-                0.39f,
-                3.7f,
-                452
-        ));
-        planets.add(new Planet(
-                "Venus",
-                "",
-                "light_blue",
-                0.723f,
-                8.87f,
-                726
-        ));
-        planets.add(new Planet(
-                "Terra",
-                "Diepenbeek",
-                "blue",
-                1f,
-                9.81f,
-                310
-        ));
-        planets.add(new Planet(
-                "Mars",
-                "Valis Marineris",
-                "red/brown",
-                1.524f,
-                3.711f,
-                150
-        ));
-        planets.add(new Planet(
-                "Jupiter",
-                "",
-                "brown",
-                5.203f,
-                24.79f,
-                120
-        ));
-        planets.add(new Planet(
-                "Saturn",
-                "",
-                "light_brown",
-                9.539f,
-                10.44f,
-                88
-        ));
-        planets.add(new Planet(
-                "Uranus",
-                "",
-                "white_blue",
-                19.18f,
-                8.87f,
-                59
-        ));
-        planets.add(new Planet(
-                "Neptune",
-                "",
-                "dark_blue",
-                30.06f,
-                11.15f,
-                48
-        ));
-        planets.add(new Planet(
-                "Pluto",
-                "",
-                "grey/brown",
-                39.53f,
-                0.62f,
-                37
-        ));
-        return planets;
-    }
 }
