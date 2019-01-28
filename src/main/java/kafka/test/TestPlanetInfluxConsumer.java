53284f8895b0a94524afdc4845d9deda8a36d2d1
Pieter Polmans <trianoware@gmail.com>
2019-01-28 13:19:19 +0100

    Added a test for planetinfluxconsumer

---
+++ src/main/java/kafka/test/TestPlanetInfluxConsumer.java
@@ -0,0 +1,9 @@
+package kafka.test;
+
+import kafka.consumers.PlanetInfluxConsumer;
+
+public class TestPlanetInfluxConsumer {
+    public static void main(String[] args){
+        new PlanetInfluxConsumer().run();
+    }
+}
