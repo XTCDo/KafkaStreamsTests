ff648ac5e3ed340c4cf03ddeee15b679e5d677f4
Pieter Polmans <trianoware@gmail.com>
2019-01-28 13:17:30 +0100

    Made an implementation of GenericThreadedInfluxConsumer

--- src/main/java/kafka/consumers/PlanetInfluxConsumer.java
+++ src/main/java/kafka/consumers/PlanetInfluxConsumer.java
@@ -1,4 +1,35 @@
 package kafka.consumers;
 
-public class PlanetInfluxConsumer {
+import kafka.generic.consumers.GenericThreadedInfluxConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.influxdb.dto.Point;
+import planets.Planet;
+
+import java.time.Duration;
+
+public class PlanetInfluxConsumer extends GenericThreadedInfluxConsumer<String, String> {
+    public PlanetInfluxConsumer(){
+        super("http://localhost:8086", "streams-planets-input","localhost:9092", "PlanetInfluxConsumer");
+    }
+
+    public void run(){
+        Thread consumerThread = new Thread(() -> {
+            try {
+                while(true){
+                    ConsumerRecords<String, String> records = getConsumer().poll(Duration.ofMillis(10));
+                    for(ConsumerRecord<String, String> record : records){
+                        Planet planet = new Planet(record.value());
+                        planet.describe();
+                        Point point = planet.toPoint();
+                        getInfluxDAO().writePoint("kafka_test", point);
+                    }
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+                getInfluxDAO().close();
+            }
+        });
+        super.run(consumerThread);
+    }
 }
