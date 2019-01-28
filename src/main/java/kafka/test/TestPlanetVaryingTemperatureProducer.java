ebd99374fcdf1c0f6ecde61382f43733b2896696
Pieter Polmans <trianoware@gmail.com>
2019-01-28 15:39:44 +0100

    Testing the planetvaryingtemperatureproducer

---
+++ src/main/java/kafka/test/TestPlanetVaryingTemperatureProducer.java
@@ -0,0 +1,9 @@
+package kafka.test;
+
+import kafka.producers.PlanetVaryingTemperatureProducer;
+
+public class TestPlanetVaryingTemperatureProducer {
+    public static void main(String[] args){
+        new PlanetVaryingTemperatureProducer().run();
+    }
+}
