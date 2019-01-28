4d09c78d18c694cb3d1c8a9fc126de31b54310b4
Pieter Polmans <trianoware@gmail.com>
2019-01-28 15:38:36 +0100

    Added PlanetVaryingTemperatureProducer

--- src/main/java/kafka/producers/PlanetVaryingTemperatureProducer.java
+++ src/main/java/kafka/producers/PlanetVaryingTemperatureProducer.java
@@ -1,4 +1,30 @@
 package kafka.producers;
 
-public class PlanetVaryingTemperatureProducer {
+import kafka.generic.producers.GenericThreadedProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import planets.Planet;
+import planets.PlanetProvider;
+
+import java.util.List;
+
+public class PlanetVaryingTemperatureProducer extends GenericThreadedProducer<String, String> {
+    public PlanetVaryingTemperatureProducer(){
+        super("streams-planets-input", "localhost:9092");
+    }
+
+    public void run(){
+        Thread producerThread = new Thread(() -> {
+            try {
+                while(true){
+                    List<Planet> planets = PlanetProvider.getPlanetsVaryingTemperature();
+                    planets.forEach(planet -> getProducer().send(new ProducerRecord<String, String>(getTopic(),
+                            planet.getName(), planet.toString())));
+                    Thread.sleep(100);
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+        super.run(producerThread);
+    }
 }
