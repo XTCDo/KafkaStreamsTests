f9bb2472876c86dba95fdce694a4edd761d3be11
Pieter Polmans <trianoware@gmail.com>
2019-01-28 16:39:09 +0100

    Switched NewPlanetConsumer with PlanetInfluxConsumer in TestPlanetProducerAndConsumer

--- src/main/java/kafka/test/TestPlanetProducerAndConsumer.java
+++ src/main/java/kafka/test/TestPlanetProducerAndConsumer.java
@@ -1,11 +1,12 @@
 package kafka.test;
 
 import kafka.consumers.NewPlanetConsumer;
+import kafka.consumers.PlanetInfluxConsumer;
 import kafka.producers.PlanetVaryingTemperatureProducer;
 
 public class TestPlanetProducerAndConsumer {
     public static void main(String[] args){
         new PlanetVaryingTemperatureProducer().run();
-        new NewPlanetConsumer().run();
+        new PlanetInfluxConsumer().run();
     }
 }
