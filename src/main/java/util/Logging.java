0d6291a158b85b5572be571c9e624a9e9ad7117d
Pieter Polmans <trianoware@gmail.com>
2019-01-30 10:42:34 +0100

    Added first logging attempt

---
+++ src/main/java/util/Logging.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class Logging {
+    static Logger logger;
+    private Handler fileHandler;
+    private Formatter plainTextFormatter;
+
+    public Logging() {
+        try {
+            logger = Logger.getLogger(Logging.class.getName());
+            fileHandler = new FileHandler("log", true);
+            plainTextFormatter = new SimpleFormatter();
+            fileHandler.setFormatter(plainTextFormatter);
+            logger.addHandler(fileHandler);
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
