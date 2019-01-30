f2b0db5bd33fa34932e2bf216761f15ee9b59a9b
Pieter Polmans <trianoware@gmail.com>
2019-01-30 15:07:29 +0100

    fixed problem with logging date

--- src/main/java/util/LoggingFormatter.java
+++ src/main/java/util/LoggingFormatter.java
@@ -17,8 +17,8 @@ public class LoggingFormatter extends Formatter {
     public String format(LogRecord record) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder//.append("[")
-                .append(new Date(dateFormat.format(record.getMillis())))
+        stringBuilder.append("[")
+                .append(dateFormat.format(new Date()))
                 .append("]")
                 .append("[")
                 .append(record.getLevel())
