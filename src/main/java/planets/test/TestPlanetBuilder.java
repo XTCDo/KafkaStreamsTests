b1b8148b3e106f8294b3415f0a5886ded48c8d68
Pieter Polmans <trianoware@gmail.com>
2019-01-30 17:14:57 +0100

    Added more comments

--- src/main/java/planets/test/TestPlanetBuilder.java
+++ src/main/java/planets/test/TestPlanetBuilder.java
@@ -10,6 +10,8 @@ import planets.exceptions.InvalidTemperatureRangeException;
 
 public class TestPlanetBuilder {
     public static void main(String[] args){
+
+        // Create a regular planet and describe it
         PlanetBuilder pb = new PlanetBuilder();
         Planet djop = pb.setName("Diepenbeek")
                 .setCapitol("Toekomststraat")
@@ -18,9 +20,10 @@ public class TestPlanetBuilder {
                 .setGravity(9.81f)
                 .setTemperature(290.0f)
                 .build();
-
         djop.describe();
 
+        // Create a PlanetVaryingTemperature and describe it
+        // 5 times to see that the temperature actually varies
         pb = new PlanetBuilder();
         Planet randomDjop = pb.setName("Diepenbeek")
                 .setCapitol("Toekomststraat")
@@ -35,6 +38,8 @@ public class TestPlanetBuilder {
             randomDjop.describe();
         }
 
+        // Create a planet with an invalid temperature to
+        // test if an InvalidTemperatureException is thrown
         try {
             pb = new PlanetBuilder();
             Planet invalidTemperaturePlanet = pb.setTemperature(-1.0f).build();
@@ -42,6 +47,8 @@ public class TestPlanetBuilder {
             ite.printStackTrace();
         }
 
+        // Create a planet with an invalid gravity to
+        // test if an InvalidGravityException is thrown
         try {
             pb = new PlanetBuilder();
             Planet invalidGravityPlanet = pb.setGravity(-1.0f).build();
@@ -49,6 +56,8 @@ public class TestPlanetBuilder {
             ige.printStackTrace();
         }
 
+        // Create a planet with an invalid distanceToSun to
+        // test if an InvalidDistanceToSunException is thrown
         try {
             pb = new PlanetBuilder();
             Planet invalidDistanceToSunPlanet = pb.setDistanceToSun(-1.0f).build();
@@ -56,6 +65,8 @@ public class TestPlanetBuilder {
             idtse.printStackTrace();
         }
 
+        // Create a planet with a minimum temperature larger than the maximum
+        // to test if an InvalidTemperatureRangeException is thrown
         try {
             pb = new PlanetBuilder();
             PlanetVaryingTemperature largerMinimumTemperaturePlanet =
@@ -66,6 +77,8 @@ public class TestPlanetBuilder {
             itre.printStackTrace();
         }
 
+        // Create a planet with an invalid minimum temperature to
+        // test if an InvalidTemperatureRangeException is thrown
         try {
             pb = new PlanetBuilder();
             PlanetVaryingTemperature invalidMinimumTemperaturePlanet =
