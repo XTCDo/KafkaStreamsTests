09caffb333507e52ad01665d16148f22ebedf0ad
Pieter Polmans <trianoware@gmail.com>
2019-01-31 14:33:09 +0100

    More logging testing

--- src/main/java/util/test/TestLoggingStreams.java
+++ src/main/java/util/test/TestLoggingStreams.java
@@ -2,11 +2,20 @@ package util.test;
 
 import util.Logging;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.logging.Level;
 
 public class TestLoggingStreams {
     public static void main(String[] args){
-        Logging.log(Level.INFO, "Starting test");
-        new TestLoggingProducer().run();
+        final String TAG = "TestLoggingStreams.main";
+        Logging.log(Level.INFO, "Creating testLoggingProducer", TAG);
+        TestLoggingProducer testLoggingProducer = new TestLoggingProducer();
+        Logging.log(Level.INFO, "Creating testLoggingConsumer");
+        TestLoggingConsumer testLoggingConsumer = new TestLoggingConsumer();
+
+        Logging.log(Level.INFO, "Starting testLoggingProducer");
+        testLoggingProducer.run();
+        Logging.log(Level.INFO, "Starting testLoggingConsumer");
+        testLoggingConsumer.run();
     }
 }
