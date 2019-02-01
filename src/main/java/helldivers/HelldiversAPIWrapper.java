56c459fe8247a42e4ee98e2c8acd24bd8f664bb2
Pieter Polmans <trianoware@gmail.com>
2019-02-01 15:22:36 +0100

    Get campaign status from helldivers api'

--- src/main/java/helldivers/HelldiversAPIWrapper.java
+++ src/main/java/helldivers/HelldiversAPIWrapper.java
@@ -2,8 +2,13 @@ package helldivers;
 
 import util.Logging;
 
+import java.io.DataOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 
 public class HelldiversAPIWrapper {
@@ -13,8 +18,34 @@ public class HelldiversAPIWrapper {
             URL url = new URL("https://api.helldiversgame.com/1.0/");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("action", "get_campaign_status");
+            connection.setDoOutput(true);
+            DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(ParameterStringBuilder.getParamsString(parameters));
+            outputStream.flush();
+            outputStream.close();
         } catch (Exception e){
             Logging.log(Level.SEVERE, e.getMessage(), TAG);
         }
+    }
+}
+
+class ParameterStringBuilder {
+    public static String getParamsString(Map<String, String> params)
+            throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            result.append("&");
+        }
+
+        String resultString = result.toString();
+        return resultString.length() > 0
+                ? resultString.substring(0, resultString.length() - 1)
+                : resultString;
     }
 }
