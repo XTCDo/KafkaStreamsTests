14bc057d998a9a68546308f19033532f81f2bd97
Chiel Dommange <chiel.dommange@gmail.com>
2019-02-07 11:48:37 +0100

    testing ObjectSerde

--- src/main/java/kafka/test/GenericStreamTest.java
+++ src/main/java/kafka/test/GenericStreamTest.java
@@ -37,7 +37,7 @@ public class GenericStreamTest {
             byte[] serializedPlanet = objectSerde.serializer().serialize("topic",planet);
 
             log("de-serializing result");
-            Planet deserializedPlanet = (Planet) objectSerde.deserializer().deserialize("topic",serializedPlanet);
+            Planet deserializedPlanet = (Planet) objectSerde.deserializer().deserialize(null, serializedPlanet);
 
             log("planet de-serialized:" + deserializedPlanet.toString());
 
