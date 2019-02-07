f80756c4ea2f7153c233ba80d91234ef355e9292
Chiel Dommange <chiel.dommange@gmail.com>
2019-02-07 11:44:38 +0100

    more fine grained testing on new classes

--- src/main/java/kafka/test/GenericStreamTest.java
+++ src/main/java/kafka/test/GenericStreamTest.java
@@ -2,8 +2,11 @@ package kafka.test;
 
 
 import kafka.generic.streams.GenericStream;
+import kafka.generic.streams.ObjectSerde;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import planets.Planet;
+import planets.PlanetBuilder;
 import util.Logging;
 
 import java.util.logging.Level;
@@ -17,12 +20,35 @@ public class GenericStreamTest {
             // first steps to setting up a stream is buildin the topic:
             // in this instance a simple pipe
 
+            log("testing ObjectSerde");
+            log("ObjectSerde setup...");
+            ObjectSerde objectSerde = new ObjectSerde();
+
+            Planet planet = new PlanetBuilder()
+                    .setName("Super-Earth")
+                    .setCapitol("UN HQ ONE")
+                    .setColor("silver")
+                    .setDistanceToSun(10f)
+                    .setGravity(1f)
+                    .setTemperature(300f).build();
+            log("created planet:" + planet.toString());
+
+            log("using ObjectSerde to serialize planet into ByteArray");
+            byte[] serializedPlanet = objectSerde.serializer().serialize("topic",planet);
+
+            log("de-serializing result");
+            Planet deserializedPlanet = (Planet) objectSerde.deserializer().deserialize("topic",serializedPlanet);
+
+            log("planet de-serialized:" + deserializedPlanet.toString());
+
             log("building simple pipe Stream:");
 
-            log("constructing toplogy with streamsBuilder...");
+            log("constructing toplogy with streamsBuilder");
             StreamsBuilder builder = new StreamsBuilder();
             builder.stream("streams-generic-input").to("streams-generic-output");
             final Topology topology = builder.build();
+
+            log("topology constructed: "+topology.describe());
 
             log("creating generic Stream with constructed topology...");
             GenericStream pipeStream = new GenericStream("streams-pipe", "localhost:9092", topology);
@@ -32,7 +58,8 @@ public class GenericStreamTest {
             pipeStream.run();
             log("pipeStream successfully started");
 
-            log("test concluded");
+
+            log("testing concluded");
         }
         catch (Throwable e){
             error(e);
