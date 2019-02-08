4b4ebbec71e1b3e252f44765bca0f215f5fc1b13
Chiel Dommange <chiel.dommange@gmail.com>
2019-02-08 16:02:12 +0100

    testing if serializer is set up correct

--- src/main/java/kafka/generic/streams/ObjectSerializer.java
+++ src/main/java/kafka/generic/streams/ObjectSerializer.java
@@ -42,6 +42,7 @@ public class ObjectSerializer implements Serializer {
             out.writeObject(object);
             //bArray = outputStream.toByteArray();
             bArray = outputStream.toByteArray();
+            Logging.log(Level.FINER,"parsed to(string): "+ outputStream.toString(), TAG);
         } catch (Exception e){
             Logging.log(Level.SEVERE, Arrays.toString(e.getStackTrace()),TAG);
         }
