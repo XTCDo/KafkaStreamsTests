7c8a9b89217545711b711296799327d87210a889
Chiel Dommange <chiel.dommange@gmail.com>
2019-02-12 15:11:36 +0100

    first iteration: printing received records

--- src/main/java/kafka/consumers/PiMessageConsumer.java
+++ src/main/java/kafka/consumers/PiMessageConsumer.java
@@ -2,10 +2,16 @@ package kafka.consumers;
 
 import kafka.generic.consumers.GenericThreadedConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import planets.Planet;
+import sun.rmi.runtime.Log;
 import util.Config;
 import util.Logging;
 
+import java.time.Duration;
+
 public class PiMessageConsumer extends GenericThreadedConsumer<String, String> {
+
     public PiMessageConsumer(){
         super("python-input", Config.getLocalBootstrapServersConfig(), "PiMessageConsumer");
     }
@@ -13,10 +19,16 @@ public class PiMessageConsumer extends GenericThreadedConsumer<String, String> {
     public void run(){
         Thread consumerThread = new Thread(() -> {
            try {
-
+               ConsumerRecords<String,String> records = getConsumer().poll(Duration.ofMillis(10));
+               records.forEach(record-> {
+                   String values = record.value();
+                   Logging.log("received message: " + values);
+               });
+               Thread.sleep(100);
            } catch (Exception e) {
                Logging.error(e);
            }
         });
+        super.run(consumerThread);
     }
 }
