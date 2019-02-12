285202d80ea13426f6972637963cbf9d418698f0
Chiel Dommange <chiel.dommange@gmail.com>
2019-02-12 15:48:31 +0100

    logging map.toString() don't rush me REEEEE

--- src/main/java/kafka/consumers/PiMessageConsumer.java
+++ src/main/java/kafka/consumers/PiMessageConsumer.java
@@ -1,32 +1,57 @@
 package kafka.consumers;
 
+import com.google.gson.Gson;
 import kafka.generic.consumers.GenericThreadedConsumer;
+import kafka.generic.consumers.GenericThreadedInfluxConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.influxdb.dto.Point;
 import planets.Planet;
 import sun.rmi.runtime.Log;
 import util.Config;
 import util.Logging;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-public class PiMessageConsumer extends GenericThreadedConsumer<String, String> {
+public class PiMessageConsumer extends GenericThreadedInfluxConsumer<String, String> {
     private static final String TAG = "PiMessageConsumer";
     public PiMessageConsumer(){
-        super("python-input", Config.getLocalBootstrapServersConfig(), "PiMessageConsumer");
+        super("localhost:8086","python-input", Config.getLocalBootstrapServersConfig(), "PiMessageConsumer");
     }
 
     public void run(){
         Thread consumerThread = new Thread(() -> {
+            Gson gson = new Gson();
             while (true) {
-                Logging.debug("fetching info", TAG);
                 try {
                    ConsumerRecords<String,String> records = getConsumer().poll(Duration.ofMillis(10));
-                   Logging.debug("fetched "+ records.count()+" records");
 
                    records.forEach(record-> {
-                       String values = record.value();
-                       Logging.log("received message: " + values, TAG);
+                       String values = record.value(); // confirmed OK -> is JSON
+                       Map map = gson.fromJson(values, Map.class);
+                       Logging.log("parsed JSON to map of size "+map.size(),TAG);
+
+                       /*
+                       Point point = Point.measurement("")
+                               .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                               .tag(map.get("mac_address").toString())
+                               .addField("name", getName())
+                               .addField("capitol", getCapitol())
+                               .addField("color", getColor())
+                               .addField("gravity", getGravity())
+                               .addField("dist_to_sun", getDistanceToSun())
+                               .addField("temperature", getTemperature())
+                               .build();
+
+
+                       getInfluxDAO().writePoint("kafka_test", point);
+                       */
+
+                       
+                       Logging.log("received message: " + map.toString(), TAG);
                    });
 
                    Thread.sleep(2000);
