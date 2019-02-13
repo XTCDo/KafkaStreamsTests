5aeb2e4f1e28f6dbbbb23d47c4444e1dfb20a369
Pieter Polmans <trianoware@gmail.com>
2019-02-13 15:43:29 +0100

    Added printing of defend- and attack_event

--- src/main/java/helldivers/test/TestHelldiversAPIWrapper.java
+++ src/main/java/helldivers/test/TestHelldiversAPIWrapper.java
@@ -11,6 +11,11 @@ public class TestHelldiversAPIWrapper {
         final String TAG = "TestHelldiversAPIWrapper";
         Map response = (Map) HelldiversAPIWrapper.doHTTPRequest("get_status");
         ArrayList campaignStatus = (ArrayList) response.get("campaign_status");
+        ArrayList defendEvents = (ArrayList)  response.get("defend_event");
+        ArrayList attackEvents = (ArrayList) response.get("attack_event");
+
         campaignStatus.forEach(faction -> Logging.log(faction.toString(), TAG));
+        defendEvents.forEach(event -> Logging.log(event.toString(), TAG));
+        attackEvents.forEach(event -> Logging.log(event.toString(), TAG));
     }
 }
