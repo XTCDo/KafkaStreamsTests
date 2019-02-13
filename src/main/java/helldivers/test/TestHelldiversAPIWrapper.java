d83273d1b7c298416b6ff21503c0b586f9b865ed
Pieter Polmans <trianoware@gmail.com>
2019-02-13 15:32:56 +0100

    added static to public void main...

--- src/main/java/helldivers/test/TestHelldiversAPIWrapper.java
+++ src/main/java/helldivers/test/TestHelldiversAPIWrapper.java
@@ -6,7 +6,7 @@ import util.Logging;
 import java.util.Map;
 
 public class TestHelldiversAPIWrapper {
-    public void main(String[] args){
+    public static void main(String[] args){
         final String TAG = "TestHelldiversAPIWrapper";
         Map returnValue = (Map) HelldiversAPIWrapper.doHTTPRequest("get_status");
         Logging.log(returnValue.toString(), TAG);
