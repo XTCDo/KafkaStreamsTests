0d97600846d7ec436451ead45036246dbab76a85
Pieter Polmans <trianoware@gmail.com>
2019-02-13 15:31:45 +0100

    Test Class for HelldiversAPIWrapper

--- src/main/java/helldivers/test/TestHelldiversAPIWrapper.java
+++ src/main/java/helldivers/test/TestHelldiversAPIWrapper.java
@@ -1,4 +1,14 @@
 package helldivers.test;
 
+import helldivers.HelldiversAPIWrapper;
+import util.Logging;
+
+import java.util.Map;
+
 public class TestHelldiversAPIWrapper {
+    public void main(String[] args){
+        final String TAG = "TestHelldiversAPIWrapper";
+        Map returnValue = (Map) HelldiversAPIWrapper.doHTTPRequest("get_status");
+        Logging.log(returnValue.toString(), TAG);
+    }
 }
