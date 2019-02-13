f7631e03186e2d7acadbeb0f72cc371295eca291
Pieter Polmans <trianoware@gmail.com>
2019-02-13 15:40:06 +0100

    Getting arraylist of the factions out of response

--- src/main/java/helldivers/test/TestHelldiversAPIWrapper.java
+++ src/main/java/helldivers/test/TestHelldiversAPIWrapper.java
@@ -3,12 +3,14 @@ package helldivers.test;
 import helldivers.HelldiversAPIWrapper;
 import util.Logging;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class TestHelldiversAPIWrapper {
     public static void main(String[] args){
         final String TAG = "TestHelldiversAPIWrapper";
-        Map returnValue = (Map) HelldiversAPIWrapper.doHTTPRequest("get_status");
-        Logging.log(returnValue.toString(), TAG);
+        Map response = (Map) HelldiversAPIWrapper.doHTTPRequest("get_status");
+        ArrayList campaignStatus = (ArrayList) response.get("campaign_status");
+        campaignStatus.forEach(faction -> Logging.log(faction.toString(), TAG));
     }
 }
