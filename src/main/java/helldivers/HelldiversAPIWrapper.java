23c5fb5293210bb4e6497b8b97fa0c590e5106bf
Pieter Polmans <trianoware@gmail.com>
2019-02-14 12:58:37 +0100

    Removed unused imports

--- src/main/java/helldivers/HelldiversAPIWrapper.java
+++ src/main/java/helldivers/HelldiversAPIWrapper.java
@@ -1,9 +1,6 @@
 package helldivers;
 
 import com.google.gson.Gson;
-import com.google.gson.internal.LinkedHashTreeMap;
-import com.google.gson.internal.LinkedTreeMap;
-import org.apache.kafka.common.metrics.Stat;
 import util.Logging;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -11,12 +8,9 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Array;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.*;
-import java.util.logging.Level;
 
 public class HelldiversAPIWrapper {
     // Link to API
