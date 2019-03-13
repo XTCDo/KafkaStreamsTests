2a197c9a3663d029359eb3c2129a2478744e627e
Pieter Polmans <trianoware@gmail.com>
2019-03-13 15:41:53 +0100

    Deserializing Vaillant Data

--- src/main/java/kafka/consumers/VaillantConsumer.java
+++ src/main/java/kafka/consumers/VaillantConsumer.java
@@ -1,12 +1,16 @@
 package kafka.consumers;
 
+import com.google.gson.Gson;
 import kafka.generic.consumers.GenericThreadedInfluxConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.influxdb.dto.Point;
 import util.Config;
 import util.Logging;
 
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class VaillantConsumer extends GenericThreadedInfluxConsumer<String, String> {
     static final String topic = "vaillant-input";
@@ -18,12 +22,15 @@ public class VaillantConsumer extends GenericThreadedInfluxConsumer<String, Stri
 
     public void run(){
         Thread consumerThread = new Thread(() -> {
+            Gson gson = new Gson();
             try {
                 while(true) {
                     ConsumerRecords<String, String> records = getConsumer().poll(Duration.ofMillis(10));
 
                     for(ConsumerRecord<String, String> record : records){
-                        Logging.log(record.toString(), TAG);
+                        //Logging.log(record.toString(), TAG);
+                        Map vaillantData = gson.fromJson(record.toString(), Map.class);
+                        Logging.log(vaillantData.toString());
                     }
                 }
             } catch (Exception e) {
