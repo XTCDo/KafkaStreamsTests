56273329b2f28c933675c3a04b977a3ab4864c2d
Pieter Polmans <trianoware@gmail.com>
2019-03-13 15:34:19 +0100

    Added test class for VaillantConsumer

--- src/main/java/kafka/test/TestVaillantConsumer.java
+++ src/main/java/kafka/test/TestVaillantConsumer.java
@@ -1,4 +1,15 @@
 package kafka.test;
 
+import kafka.consumers.VaillantConsumer;
+import util.Logging;
+
 public class TestVaillantConsumer {
+    private static final String TAG = "TestVaillantConsumer";
+
+    public static void main(String[] args){
+        Logging.log("Starting VaillantConsumer test", TAG);
+        VaillantConsumer vc = new VaillantConsumer();
+        vc.run();
+        Logging.log("Done testing VaillantConsumer", TAG);
+    }
 }
