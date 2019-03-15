882e498c83ef5e726821a0488cb2cc20ec966c23
Pieter Polmans <trianoware@gmail.com>
2019-03-15 12:06:17 +0100

    Added a test class for HelldiversConsumer

---
+++ src/main/java/kafka/test/TestHelldiversConsumer.java
@@ -0,0 +1,10 @@
+package kafka.test;
+
+import kafka.consumers.HelldiversConsumer;
+
+public class TestHelldiversConsumer {
+
+    public static void main(String[] args) {
+        new HelldiversConsumer().run();
+    }
+}
