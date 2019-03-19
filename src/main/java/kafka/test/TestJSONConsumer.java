f10b66daf33f3d66f01d797c31fbd1889a86293f
Chiel Dommange <chiel.dommange@gmail.com>
2019-03-19 15:34:11 +0100

    added .run() to test code

--- src/main/java/kafka/test/TestJSONConsumer.java
+++ src/main/java/kafka/test/TestJSONConsumer.java
@@ -6,6 +6,6 @@ import util.Config;
 public class TestJSONConsumer {
     public static void main(String ...args){
         new GenericJSONConsumer(Config.getLocalInfluxUrl(),"test-json-input", Config.getLocalBootstrapServersConfig(), "generic-json-consumer-test",
-                "Testing","generic-json-test");
+                "Testing","generic-json-test").run();
     }
 }
