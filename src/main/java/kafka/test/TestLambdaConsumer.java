a06e2e821d3f0e22cb2e67c0826069553122a7dc
Chiel Dommange <chiel.dommange@gmail.com>
2019-03-20 15:26:42 +0100

    debugging

--- src/main/java/kafka/test/TestLambdaConsumer.java
+++ src/main/java/kafka/test/TestLambdaConsumer.java
@@ -2,6 +2,7 @@ package kafka.test;
 
 import kafka.generic.consumers.GenericRunnableConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import sun.rmi.runtime.Log;
 import util.Config;
 import util.Logging;
 
@@ -9,11 +10,12 @@ import java.util.function.Consumer;
 
 public class TestLambdaConsumer {
     public static void main(String ...args){
-
+        Logging.debug("setting up java consumer");
         Consumer<ConsumerRecords> cons = consumerRecords -> consumerRecords.forEach(record-> Logging.log(record.toString()));
-
-        new GenericRunnableConsumer("test-input", Config.getLocalBootstrapServersConfig(), "generic-runnable-consumer", cons).run();
-
+        Logging.debug("constructing generic runnable consumer");
+        GenericRunnableConsumer test = new GenericRunnableConsumer("test-input", Config.getLocalBootstrapServersConfig(), "generic-runnable-consumer", cons);
+        Logging.debug("running constructed consumer");
+        test.run();
     }
 }
 
