4724dc9c78413be4d277db73c69a39fad0987ec6
Chiel Dommange <chiel.dommange@gmail.com>
2019-03-25 11:29:53 +0100

    added consumer for campaign statuses

--- src/main/java/kafka/test/TestHelldiversConsumer.java
+++ src/main/java/kafka/test/TestHelldiversConsumer.java
@@ -1,6 +1,7 @@
 package kafka.test;
 
 import com.google.gson.Gson;
+import helldivers.CampaignStatus;
 import helldivers.Statistics;
 import kafka.generic.consumers.GenericRunnableInfluxConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -11,6 +12,7 @@ import util.Config;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 public class TestHelldiversConsumer {
@@ -26,13 +28,12 @@ public class TestHelldiversConsumer {
             List<Point> batch = new ArrayList<>();
 
             for (ConsumerRecord<String, String> record : consumerRecords) {
-                //
+                // JSON to objects
                 List<Map> statisticsList = gson.fromJson(record.value(), List.class);
 
                 for (Map statistics : statisticsList) {
                     // todo hacky fix, find different way
                     long timeStamp = (long) Math.round((double) statistics.get("timeStamp"));
-
                     statistics.put("timeStamp", timeStamp);
 
                     Statistics statisticsObject = new Statistics(statistics);
@@ -49,27 +50,43 @@ public class TestHelldiversConsumer {
             List<Point> batch = new ArrayList<>();
 
             for (ConsumerRecord<String, String> record : consumerRecords) {
-                //
-                List<Map> campaignStatusList = gson.fromJson(record.value(), List.class);
+                // json to Object casting
+                List<CampaignStatus> campaignStatuses = gson.fromJson(record.value(), List.class);
 
-                for (Map status : campaignStatusList) {
-                    // todo hacky fix, find different way
-                    long timeStamp = Double.valueOf(status.get("timeStamp")).longValue();
-
-                    statistics.put("timeStamp", timeStamp);
-
-                    Statistics statisticsObject = new Statistics(statistics);
-                    batch.add(statisticsObject.toPoint());
+                // parsing objects
+                for (CampaignStatus cstat: campaignStatuses) {
+                    // parse campaign status object to Influx point
+                    Point point = Point.measurement("helldivers-campaign-status")
+                            .time(cstat.getTimeStamp(), TimeUnit.MILLISECONDS)
+                            .tag("season", String.valueOf(cstat.getSeason()))
+                            .tag("enemy", cstat.getEnemyName())
+                            .tag("status", cstat.getStatus())
+                            .tag("introduction_order", String.valueOf(cstat.getIntroductionOrder()))
+                            .addField("points", cstat.getPoints())
+                            .addField("points_max", cstat.getPointsMax())
+                            .addField("points_taken", cstat.getPointsTaken())
+                            .build();
+                    // add to buffer
+                    batch.add(point);
                 }
             }
 
+            // return the buffer so the InfluxDao can insert it into it's database
             return batch;
         };
 
+        // consuming statistics
         new GenericRunnableInfluxConsumer(
                 "http://localhost:8086", "HELLDIVERS",
-                "helldivers-statistics", Config.getLocalBootstrapServersConfig(), "HelldiversConsumer",
+                "helldivers-statistics", Config.getLocalBootstrapServersConfig(), "HelldiversStatisticsConsumer",
                 statisticsToPointBatch)
+                .run();
+
+        // consuming campaign statuses
+        new GenericRunnableInfluxConsumer(
+                "http://localhost:8086", "HELLDIVERS",
+                "helldivers-campaign-status", Config.getLocalBootstrapServersConfig(), "HelldiversCampaignStatusConsumer",
+                StatusesToPointBatch)
                 .run();
 
 
