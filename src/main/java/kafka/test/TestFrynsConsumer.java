d5994650d264666c8dfbe655da692d857a9f38cf
do <trianoware@gmail.com>
2019-03-26 13:19:56 +0100

    Consumer for fryns data

---
+++ src/main/java/kafka/test/TestFrynsConsumer.java
@@ -0,0 +1,39 @@
+package kafka.test;
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import kafka.generic.consumers.GenericRunnableInfluxConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.influxdb.dto.Point;
+import util.Config;
+import util.MapUtils;
+
+public class TestFrynsConsumer {
+
+    public static void main(String[] args) {
+        Function<ConsumerRecords<String, String>, List<Point>> frynsDataToPointBatch = consumerRecords -> {
+            Gson gson = new Gson();
+            List<Point> batch = new ArrayList<>();
+
+            for (ConsumerRecord<String, String> record : consumerRecords) {
+                List<Map> frynsDataList = gson.fromJson(record.value(), List.class);
+
+                for (Map frynsData : frynsDataList ) {
+                    batch.add(MapUtils.influxMapToPoint(frynsData, "fryns_data"));
+                }
+            }
+
+            return batch;
+        };
+
+        new GenericRunnableInfluxConsumer(
+            "https://localhost:8086", "fryns",
+            "fryns-input", Config.getLocalBootstrapServersConfig(), "Fryns",
+            frynsDataToPointBatch
+        ).run();
+    }
+}
